f95a93c0095f384ee07573e73e635d3b51ea0b71
erfan._.teikst <erfan.teist2027@gmail.com>
2024-10-02 20:14:08 +0330

    setup zustand to use

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { useBearStore } from "./store/store";
 
 function App() {
   const [count, setCount] = useState(0);
+  const bears = useBearStore((state) => state.bears);
+  const increasePopulation = useBearStore((state) => state.increasePopulation);
 
   return (
     <div className="text-center">
       <h1 className="text-4xl font-bold text-blue-500">
         Hello, Tailwind in React!
       </h1>
+      <h1>{bears} bears around here ...</h1>
+      <button
+        class="bg-red-700 rounded-2xl w-48 h-36"
+        onClick={() => increasePopulation()}
+      >
+        +
+      </button>
     </div>
   );
 }
