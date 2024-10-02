d5594509528a2b0b56be4590922b1a0bed049b3e
erfan._.teikst <erfan.teist2027@gmail.com>
2024-10-02 20:51:01 +0330

    fix some tailwind issuses and add custom_styles.css file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "./custom_styles.css";
 import { useBearStore } from "./store/store";
 
 function App() {
@@ -10,13 +11,13 @@ function App() {
   const increasePopulation = useBearStore((state) => state.increasePopulation);
 
   return (
-    <div className="text-center">
+    <div className="card text-center w-1/2 mx-auto">
       <h1 className="text-4xl font-bold text-blue-500">
         Hello, Tailwind in React!
       </h1>
       <h1>{bears} bears around here ...</h1>
       <button
-        class="bg-red-700 rounded-2xl w-48 h-36"
+        class="bg-red-700 rounded-2xl w-10"
         onClick={() => increasePopulation()}
       >
         +
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,15 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useBearStore = create((set) => ({
-  bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-}));
+export const useBearStore = create(
+  persist(
+    (set) => ({
+      bears: 0,
+      increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+      removeAllBears: () => set({ bears: 0 }),
+    }),
+    {
+      name: "bear-storage", // name of the item in the storage (must be unique)
+    }
+  )
+);
